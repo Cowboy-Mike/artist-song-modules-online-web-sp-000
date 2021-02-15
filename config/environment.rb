4da1d4a5ef55d65b0607f5e7012b61439c37cd1c
Mike Nanto <mike@thecowboymechanic.com>
2021-02-15 01:04:25 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -1,4 +1,4 @@
-require_relative '../lib/concerns/findable'
+#require_relative '../lib/concerns/findable'
 require_relative '../lib/concerns/memorable'
 require_relative '../lib/concerns/paramable'
 require_relative '../lib/artist'
